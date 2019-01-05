dc35ca3e851854c5f6df38aa24fe3821f5749698
Aditya Bansal <eadityabansal@gmail.com>
2019-01-06 01:30:14 +0530

    testing view 3

--- views/site/home.php
+++ views/site/home.php
@@ -2,22 +2,21 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Aditya\'s Blog';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Aditya bansal</h1>
+        <h1>Aditya's Blog</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
-            
+
         </div>
 
     </div>
