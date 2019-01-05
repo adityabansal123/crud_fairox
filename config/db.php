b6803104802cfa78706fd2ebab3138b1ae46556d
Aditya Bansal <eadityabansal@gmail.com>
2019-01-06 01:18:46 +0530

    configuring db creds

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=fairohkq_crud',
+    'username' => 'fairohkq_crud',
+    'password' => 'Mamta2014@',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
